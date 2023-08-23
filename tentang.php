08f8333017926fd066816feb7406d94a94dc1d65
crophx <azmiazis708@gmail.com>
2023-08-23 23:08:42 +0700

    ubah Selamat Datang

---
+++ tentang.php
@@ -0,0 +1,218 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Tentang - azmi</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="assets/css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
+            <div class="container">
+                <a class="navbar-brand" href="#page-top">Selamat Datang</a>
+                <button class="navbar-toggler text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    Menu
+                    <i class="fas fa-bars"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="index.html">Beranda</a></li>
+                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="portfolio.html">Portofolio</a></li>
+                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="tentang.html">Tentang</a></li>
+                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="kontak.html">Kontak</a></li>
+                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="pendidikan.html">Pendidikan</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Masthead-->
+        <header class="masthead bg-primary text-white text-center">
+            <div class="container d-flex align-items-center flex-column">
+                <!-- Masthead Avatar Image-->
+                <img class="masthead-avatar mb-5" src="assets/img/avataaars.svg" alt="..." />
+                <!-- Masthead Heading-->
+                <h1 class="masthead-heading text-uppercase mb-0">Tentang</h1>
+                <!-- Icon Divider-->
+                <div class="divider-custom divider-light">
+                    <div class="divider-custom-line"></div>
+                    <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                    <div class="divider-custom-line"></div>
+                </div>
+                <!-- Masthead Subheading-->
+                <p class="masthead-subheading font-weight-light mb-0">Halaman ini berisikan sekilas tentang kehidupan saya</p>
+            </div>
+        </header>
+        
+        <!-- About Section-->
+        <section class="page-section bg-white text-black mb-0" id="about">
+            <div class="container">
+                <!-- About Section Heading-->
+                <h2 class="page-section-heading text-center text-uppercase text-secondary">Aktivitas</h2>
+                
+                <!-- Icon Divider-->
+                <div class="divider-custom">
+                    <div class="divider-custom-line"></div>
+                    <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                    <div class="divider-custom-line"></div>
+                    
+                </div>
+                <span class="text-center">Post by : Azmi Azis </span>
+                <!-- About Section Content-->
+                <table class="table">
+                    <thead>
+                        <tr class="fs-2 text-secondary">
+                            <th>Mata Kuliah</th>
+                            <th>Hari</th>
+                            <th>Jam</th>
+                            <th>SKS</th>
+                            <th>Ruangan</th>				
+                        </tr>
+                    </thead>
+                    <tbody class="fs-5">
+                        <tr>
+                            <td>Pemrograman Objek</td>
+                            <td>Senin</td>
+                            <td>08.00 - 09.40</td>
+                            <td>2</td>
+                            <td>Lab Dasar</td>
+                        </tr>
+                        <tr>
+                            <td>Metodologi Penelitian Ilmu Komputer</td>
+                            <td>Senin</td>
+                            <td>10.00 - 11.40</td>
+                            <td>2</td>
+                            <td>910</td>
+                        </tr>
+                        <tr>
+                            <td>Pemrograman Web</td>
+                            <td>Senin</td>
+                            <td>13.00 - 15.30</td>
+                            <td>3</td>
+                            <td>Lab Dasar</td>
+                        </tr>
+                        <tr>
+                            <td>Manajemen Teknik</td>
+                            <td>Selasa</td>
+                            <td>08.00 - 09.40</td>
+                            <td>2</td>
+                            <td>404b</td>
+                        </tr>
+                        <tr>
+                            <td>Basis Data</td>
+                            <td>Selasa</td>
+                            <td>10.00 - 12.30</td>
+                            <td>3</td>
+                            <td>Lab Simpel</td>
+                        </tr>
+                        <tr>
+                            <td>Sistem Multimedia</td>
+                            <td>Selasa</td>
+                            <td>13.00 - 14.40</td>
+                            <td>2</td>
+                            <td>Lab Multi</td>
+                        </tr>
+                        <tr>
+                            <td>Struktur Data</td>
+                            <td>Rabu</td>
+                            <td>08.00 - 09.40</td>
+                            <td>2</td>
+                            <td>Lab SAQ</td>
+                        </tr>
+                        <tr>
+                            <td>Sistem Operasi</td>
+                            <td>Rabu</td>
+                            <td>10.00 - 09.40</td>
+                            <td>2</td>
+                            <td>404b</td>
+                        </tr>
+                        <tr>
+                            <td>Pemrosesan Data Terdistribusi</td>
+                            <td>Rabu</td>
+                            <td>13.00 - 14.40</td>
+                            <td>2</td>
+                            <td>904</td>
+                        </tr>
+                        <tr>
+                            <td>Teknik Digital</td>
+                            <td>Kamis</td>
+                            <td>08.00 - 09.40</td>
+                            <td>2</td>
+                            <td>Lab Linux</td>
+                        </tr>
+                        <tr>
+                            <td>Komunikasi Data</td>
+                            <td>Kamis</td>
+                            <td>13.00 - 14.40</td>
+                            <td>2</td>
+                            <td>910</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <!-- About Section Button-->
+                <div class="text-center mt-4">
+                    <a class="btn btn-xl btn-outline-primary" href="pendidikan.html">
+                        <i class="fas fa-meh me-2"></i>
+                        klik disini untuk halaman selanjutnya
+                    </a>
+                </div>
+            </div>
+        </section>
+        
+        <!-- Footer-->
+        <!-- Footer-->
+        <footer class="footer text-center">
+            <div class="container">
+                <div class="row">
+                    <!-- Footer Domisili-->
+                    <div class="col-lg-4 mb-5 mb-lg-0">
+                        <h4 class="text-uppercase mb-4">Domisili</h4>
+                        <p class="lead mb-0">
+                            Kota Administrasi
+                            <br />
+                            Jakarta Barat, Indonesia
+                        </p>
+                    </div>
+                    <!-- Footer Social Icons-->
+                    <div class="col-lg-4 mb-5 mb-lg-0">
+                        <h4 class="text-uppercase mb-4">Bagian Footer</h4>
+                        <a class="btn btn-outline-light btn-social mx-1" href="https://www.instagram.com/crophx/"><i class="fab fa-fw fa-instagram"></i></a>
+                        <a class="btn btn-outline-light btn-social mx-1" href="https://twitter.com/kubukandilanmu"><i class="fab fa-fw fa-twitter"></i></a>
+                        <a class="btn btn-outline-light btn-social mx-1" href="https://www.linkedin.com/in/azmiazis/"><i class="fab fa-fw fa-linkedin-in"></i></a>
+                        <a class="btn btn-outline-light btn-social mx-1" href="=https://www.facebook.com/alienx2K20"><i class="fab fa-fw fa-facebook"></i></a>
+                    </div>
+                    <!-- Footer About Text-->
+                    <div class="col-lg-4">
+                        <h4 class="text-uppercase mb-4">Tentang Web ini</h4>
+                        <p class="lead mb-0">
+                            Web ini dibuat dengan menggunaakn boostrap 5, ada beberapa element yang di copy melalui templete dan ada beberapa juga yang dibuat dengan codingan murni
+                        </p>
+                    </div>
+                </div>  
+            </div>
+        </footer>
+        <!-- Copyright Section-->
+        <div class="copyright py-4 text-center text-white">
+            <div class="container"><small>Copyright &copy; Your Website 2022</small></div>
+        </div>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <!-- * *                               SB Forms JS                               * *-->
+        <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+    </body>
+</html>
